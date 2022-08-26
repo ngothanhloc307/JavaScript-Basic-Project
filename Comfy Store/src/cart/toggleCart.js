7ff2540ec53ac0b836233ba268ddfa804a12132e
NGOLOC-T470P\Locnt <ngothanhloc90@gmail.com>
2022-08-26 21:23:43 +0700

    complete all all page method

--- Comfy Store/src/cart/toggleCart.js
+++ Comfy Store/src/cart/toggleCart.js
@@ -1,3 +1,18 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+
+
+const cartOverlay = getElement('.cart-overlay');
+const closecartBtn = getElement('.cart-close');
+const toggleCartBtn = getElement('.toggle-cart');
+
+toggleCartBtn.addEventListener('click', () => {
+    cartOverlay.classList.add('show');
+});
+
+closecartBtn.addEventListener('click', () => {
+    cartOverlay.classList.remove('show');
+})
+export const openCart = () => {
+    cartOverlay.classList.add('show');
+};
--- Comfy Store/src/filters/companies.js
+++ Comfy Store/src/filters/companies.js
@@ -1,6 +1,42 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+    let companies = ['all', ...new Set(store.map(product => product.company))];
+    const companiesDOM = getElement('.companies');
+    companiesDOM.innerHTML = companies.map(company => {
+        return `<button class="company-btn">${company}</button>`;
+    }).join('');
+    companiesDOM.addEventListener('click', (e) => {
+        const element = e.target;
+        if (element.classList.contains('company-btn')) {
+
+            let newStore = [];
+            if (element.textContent === 'all') {
+                newStore = [...store];
+            } else {
+                newStore = store.filter(product => product.company === e.target.textContent);
+            }
+            display(newStore, getElement('.products-container'));
+        }
+    })
+
+    const btnCategories = document.querySelectorAll(".company-btn");
+
+    btnCategories.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            // console.log(btn);
+
+            btnCategories.forEach(function (item) {
+                if (item !== btn) {
+                    item.classList.remove("show-borber-button");
+                }
+            });
+
+            btn.classList.toggle("show-borber-button");
+        });
+    })
+};
 
 export default setupCompanies;
+
